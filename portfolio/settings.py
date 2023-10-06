ff56f9a3e9d21a8f00c92203deffb2fd1d3390bc
Dat Tran <datttrian@gmail.com>
2023-10-06 19:13:09 +0000

    Adding static images to your Django project

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4uh!pzj55rb_g407q_8wmi#qr-$88)o#cfpoqhb(%tn_#n=p6q'
+SECRET_KEY = (
+    'django-insecure-4uh!pzj55rb_g407q_8wmi#qr-$88)o#cfpoqhb(%tn_#n=p6q'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
